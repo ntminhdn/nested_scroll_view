b6086a52e1d39195e4e2147ebe272552584abe62
nguyenthanhminhb <nguyen.thanh.minhb@framgia.com>
2023-02-23 20:26:14 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/nested_scroll_view_tab_bar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nested_scroll_view_tab_bar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WhatsAppAppbar());
+}
+
+class WhatsAppAppbar extends StatelessWidget {
+  const WhatsAppAppbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> tabs = <String>['Tab 1', 'Tab 2', 'Tab 3', 'Tab 4'];
+
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 4,
+        child: SafeArea(
+          child: Container(
+            constraints: const BoxConstraints.expand(),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/img/bg.jpg"), fit: BoxFit.cover),
+            ),
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              body: NestedScrollView(
+                floatHeaderSlivers: true,
+                headerSliverBuilder:
+                    (BuildContext context, bool innerBoxIsScrolled) {
+                  return <Widget>[
+                    const SliverAppBar(
+                      title: Text("Whatsapp"),
+                      centerTitle: false,
+                      automaticallyImplyLeading: false,
+                      floating: true,
+                      backgroundColor: Color.fromARGB(255, 4, 94, 84),
+                      actions: [
+                        Icon(Icons.search, size: 30, color: Colors.white),
+                        SizedBox(width: 10),
+                        Icon(Icons.more_vert, size: 30, color: Colors.white),
+                      ],
+                      elevation: 0.0,
+                    ),
+                    const SliverToBoxAdapter(
+                      child: SizedBox(
+                        height: 5,
+                      ),
+                    ),
+                    SliverPersistentHeader(
+                      pinned: true,
+                      delegate: WhatsappTabs(50.0),
+                    ),
+                  ];
+                },
+                body: TabBarView(
+                  children: tabs.map(
+                    (String name) {
+                      return const CustomScrollView(
+                        slivers: [
+                          Messages(),
+                        ],
+                      );
+                    },
+                  ).toList(),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WhatsappTabs extends SliverPersistentHeaderDelegate {
+  final double size;
+
+  WhatsappTabs(this.size);
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: const Color.fromARGB(255, 4, 94, 84),
+      height: size,
+      child: const TabBar(
+        indicatorWeight: 3,
+        indicatorColor: Colors.white,
+        labelColor: Colors.white,
+        unselectedLabelColor: Colors.white60,
+        // isScrollable: true,
+        tabs: <Widget>[
+          Tab(icon: Icon(Icons.camera_alt)),
+          Tab(text: "Chats"),
+          Tab(text: "Status"),
+          Tab(text: "Calls"),
+        ],
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => size;
+
+  @override
+  double get minExtent => size;
+
+  @override
+  bool shouldRebuild(WhatsappTabs oldDelegate) {
+    return oldDelegate.size != size;
+  }
+}
+
+class Messages extends StatelessWidget {
+  const Messages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          if (index > 15) {
+            return null;
+          }
+
+          return const ListTile(
+            contentPadding: EdgeInsets.all(16),
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEgzwHNJhsADqquO7m7NFcXLbZdFZ2gM73x8I82vhyhg&s"),
+            ),
+            title: Text("Mr. H"),
+            subtitle: Text("Hey there, look at the appbar"),
+          );
+        },
+      ),
+    );
+  }
+}
